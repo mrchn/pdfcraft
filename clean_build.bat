feabc9b2f4e3780eb49964e7726d48ba428bb42c
mrchn <mrnfll@icloud.com>
2026-04-19 04:20:18 +0300

    0-1-3 bug fixes and minor improvements

--- clean_build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo cleaning old builds...
-if exist build rmdir /s /q build
-if exist dist rmdir /s /q dist
-if exist pdfcraft_mrchn.egg-info rmdir /s /q pdfcraft_mrchn.egg-info
-echo done!
